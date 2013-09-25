dbba758746a33fa5efb511767e1a31b99110b17a
ChocoboMaster <cabourgeois8@gmail.com>
2013-09-25 14:40:32 -0400

    Index buttons

--- index.php
+++ index.php
@@ -15,20 +15,18 @@
 	?>
 		<div class="widget">
 			<?php echo getServerAddress(); echo ":"; echo getServerPort(); ?> 0/<?php echo getMaxPlayers(); ?>
-			<a class="btn btn-danger"><i class="icon-off icon-white"></i><br/>Arreter</a>
-			<a class="btn btn-warning"><i class="icon-repeat icon-white"></i><br/>Redemarrer</a>
 
 		</div>
 		
 		<div class="widget">
 			<h4>Liens Utiles</h4>
-			<a class="btn w50"><i class="icon-user"></i><br/>Joueurs</a>
-			<a class="btn w50"><i class="icon-th"></i><br/>Plugins</a>
-			<a class="btn w50"><i class="icon-hdd"></i><br/>Backups</a>
-			<a class="btn w50"><i class="icon-folder-open"></i><br/>FTP</a>
-			<a class="btn w50"><i class="icon-cog"></i><br/> Propri&eacute;t&eacute;es</a>
-			<a class="btn w50"><i class="icon-off"></i><br/>Arr&ecirc;ter</a>
-			<a class="btn w50"><i class="icon-repeat"></i><br/>Red&eacute;marrer</a>
+			<a class="btn w50" href="joueurs.php"><i class="icon-user"></i><br/>Joueurs</a>
+			<a class="btn w50" href="plugins.php"><i class="icon-th"></i><br/>Plugins</a>
+			<a class="btn w50" href="backups.php"><i class="icon-hdd"></i><br/>Backups</a>
+			<a class="btn w50" href="#"><i class="icon-folder-open"></i><br/>FTP</a>
+			<a class="btn w50" href="settings.php"><i class="icon-cog"></i><br/> Propri&eacute;t&eacute;es</a>
+			<a class="btn btn-danger w50"><i class="icon-off icon-white"></i><br/>Arr&ecirc;ter</a>
+			<a class="btn btn-warning w50"><i class="icon-repeat icon-white"></i><br/>Red&eacute;marrer</a>
 		</div>
 		
 		<div class="widget">
